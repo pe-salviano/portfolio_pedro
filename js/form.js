d4a4f1b5822fd22a7dcad5dadc61324a08186033
pe-salviano <pedro_s.c@hotmail.com>
2022-09-13 17:16:15 -0300

    Criação da função addloadin e removeloading

--- js/form.js
+++ js/form.js
@@ -1,18 +1,33 @@
+// 
+const botaoEnviar = document.querySelector('.botao-enviar')
+
+//Adciona o loading ao clicar no botão
+const addloading = () => {
+    botaoEnviar.innerHTML = '<img class="loading" src="./assets/imagem/loading-caracol.gif" alt="">';
+}
+
+// Remove o loading e muda para enviado 
+const removeloading = () => {
+    botaoEnviar.innerHTML = 'Enviado com sucesso';
+    
+}
+
+
 const enviar = (event) => {
     event.preventDefault();
+    addloading();
 
     const name = document.querySelector('input[name=name]').value;
     const email = document.querySelector('input[name=email]').value;
     const mensagem = document.querySelector('textarea[name=mensagem]').value;
 
     fetch ('https://api.sheetmonkey.io/form/dS9euxjKVX7K3ewKb6stMr', {
-
         method: 'post',
         headers: {
             'Accept':'application/json',
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({name, email, mensagem}),
-    })
+    }).then(() => removeloading());
 }
 document.querySelector('form').addEventListener('submit', enviar);
